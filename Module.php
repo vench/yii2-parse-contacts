9b54aeff2d77c4d9f96ff85fe0634b6749923021
vench <nowwrit@mail.ru>
2017-08-06 15:55:48 +0300

    Add boot

--- Module.php
+++ Module.php
@@ -1,14 +1,28 @@
 <?php
  
 namespace Vench\ParseContacts;
+
+use yii\base\BootstrapInterface;
+
+
 /**
  * Base module class for Krajee extensions
  *
  * @author Kartik Visweswaran <kartikv2@gmail.com>
  * @since 1.8.9
  */
-class Module extends \yii\base\Module
+class Module extends \yii\base\Module implements BootstrapInterface
 {
+  
+  
+     /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app) {
+    
+        var_dump(__METHOD__);
+    }
   
     /**
      * @inheritdoc

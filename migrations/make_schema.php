181db9b5486a3ac1c98fb5743d1c552934b868c1
vench <nowwrit@mail.ru>
2017-08-06 15:13:47 +0300

    Add migrate

---
+++ migrations/make_schema.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Migration;
+
+class make_schema extends Migration
+{
+    public function safeUp()
+    {
+    
+    }
+    
+    public function safeDown()
+    {
+    
+    } 
+
+}

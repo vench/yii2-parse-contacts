6e653fd1518ce4d052a111c893b82d3c0e37a126
vench <nowwrit@mail.ru>
2017-08-13 21:36:52 +0300

    1308+++++++++++++++

--- parsers/PregMathParse.php
+++ parsers/PregMathParse.php
@@ -20,6 +20,30 @@ class PregMathParse extends AbParser
      */
     public function parse(PCSite $model)
     {
-        // TODO: Implement parse() method.
+        $content = $this->getContent($model->site);
+        $dom = new DOMDocument;
+        $dom->loadHTML($content);
+        foreach ($dom->getElementsByTagName('a') as $node) {
+            echo $dom->saveHtml($node), PHP_EOL;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $url
+     * @return bool|string
+     */
+    private function getContent($url) {
+        $opts = [
+            "http" => [
+                "method" => "GET",
+                "header" => "Accept-language: en\r\n"
+            ]
+        ];
+
+        $context = stream_context_create($opts);
+
+        return file_get_contents($url, false, $context);
     }
 }
